1dbded87a48864b2e148d051f1757833d9cb7fb5
yiqiandewo <1429711247@qq.com>
2021-04-01 22:16:54 +0800

    --

--- src/test/java/com/yiqiandewo/BlogApplicationTests.java
+++ src/test/java/com/yiqiandewo/BlogApplicationTests.java
@@ -3,15 +3,11 @@ package com.yiqiandewo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
 @SpringBootTest
 class BlogApplicationTests {
 
     @Test
     void test() {
-        Deque<Integer> stack = new LinkedList<>();
-        stack.peekLast();
+
     }
 }
